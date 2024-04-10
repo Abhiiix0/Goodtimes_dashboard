c3383f6eb7a2eae9d7c46a4964ba592ff8a31c14
Abhiiix0 <94632730+Abhiiix0@users.noreply.github.com>
2024-04-10 18:46:11 +0530

    Delete src directory

--- src/ReduxApi/Store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cartreducer from "./AddToCart";
-import rootUser from "./authSlice";
-export const Store = configureStore({
-  reducer: {
-    cart: cartreducer,
-    auth: rootUser,
-  },
-});
